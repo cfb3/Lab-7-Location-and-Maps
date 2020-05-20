f82dd0e559546e8b14a7d57c58e328e1ff7de405
Charles Bryan <cfb3@uw.edu>
2020-05-19 21:07:15 -0700

    Initial commit

---
+++ app/src/main/java/edu/uw/tcss450/lab7_locationandmaps/ui/LocationFragment.java
@@ -0,0 +1,29 @@
+package edu.uw.tcss450.lab7_locationandmaps.ui;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import edu.uw.tcss450.lab7_locationandmaps.R;
+
+
+public class LocationFragment extends Fragment {
+
+
+    public LocationFragment() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_location, container, false);
+    }
+}
